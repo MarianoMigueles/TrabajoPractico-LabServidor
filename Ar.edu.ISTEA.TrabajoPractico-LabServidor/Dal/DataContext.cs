2183278d145e45b520f8b49efac5ed417acf2f4c
MarianoMigueles <164581983+MarianoMigueles@users.noreply.github.com>
2024-08-26 17:28:25 -0300

    Creacion de Contexto de Base de Datos

---
+++ Ar.edu.ISTEA.TrabajoPractico-LabServidor/Dal/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities;
+
+namespace Ar.edu.ISTEA.TrabajoPractico_LabServidor.Dal
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public virtual DbSet<Empleados> Empleados { get; set; }
+        public virtual DbSet<Comandas> Comandas { get; set; }
+        public virtual DbSet<Mesas> Mesas { get; set; }
+        public virtual DbSet<Pedidos> Pedidos { get; set; }
+        public virtual DbSet<Productos> Productos { get; set; }
+
+    }
+}
